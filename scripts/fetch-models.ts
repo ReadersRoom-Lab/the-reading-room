caa8aff5ae4ac1919d13b91a729ffcb25c1966e6
Lokeshwaran V R <vrlokeshwaran@gmail.com>
2026-07-02 20:18:54 +0530

    chore: commit deletion of useless and unused scripts and docs

--- scripts/fetch-models.ts
+++
@@ -1,7 +0,0 @@
-async function run() {
-  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.GOOGLE_GENERATIVE_AI_API_KEY}`)
-  const json = await res.json()
-  const models = json.models.map((m: any) => m.name)
-  console.log('Available models:', models.filter((m: any) => m.includes('embed')))
-}
-run()
